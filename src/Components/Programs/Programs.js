653d2c40b439b8f63d61573cd8ab6dbf9385e941
Istiakahmed180 <istiakahmed180@gmail.com>
2023-01-02 00:02:11 +0600

    programs components added

--- src/Components/Programs/Programs.js
+++ src/Components/Programs/Programs.js
@@ -1,14 +1,31 @@
 import React from "react";
 import "./Programs.css";
+import { programsData } from "../../../src/data/programsData";
+import right_arrow from "../../assets/rightArrow.png";
 
 const Programs = () => {
   return (
     <div className="programs" id="programs">
       {/* header */}
       <div className="programs-header">
-        <span>Explore our</span>
+        <span className="text-stroke">Explore our</span>
         <span>programs</span>
-        <span>to shape you</span>
+        <span className="text-stroke">to shape you</span>
+      </div>
+
+      {/* programs data */}
+      <div className="program-categories">
+        {programsData.map((program, i) => (
+          <div className="category" key={i}>
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now </span>
+              <img src={right_arrow} alt="" />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
