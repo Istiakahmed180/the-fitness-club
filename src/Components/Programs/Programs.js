da8a63bd4c603b9af6788e4429a04a7e157b5b3c
Istiakahmed180 <Istiakahmed180@gmail.com>
2023-01-01 18:45:54 +0600

    programs components working now

---
+++ src/Components/Programs/Programs.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Programs.css";
+
+const Programs = () => {
+  return (
+    <div className="programs" id="programs">
+      {/* header */}
+      <div className="programs-header">
+        <span>Explore our</span>
+        <span>programs</span>
+        <span>to shape you</span>
+      </div>
+    </div>
+  );
+};
+
+export default Programs;
